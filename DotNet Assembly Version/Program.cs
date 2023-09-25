f79eec5601a64c70d9b0e0f400aa7ccaca302c4a
Kulpot <kulpotwahu@gmail.com>
2023-09-26 05:17:03 +0800

    Add project files.

---
+++ DotNet Assembly Version/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+
+//ref link:https://www.youtube.com/watch?v=L0UavAtVnD4&list=PLRwVmtr-pp06rfSgNYu_oBg40DkwXiRHt&index=15
+//
+
+[assembly: AssemblyVersion("1.0.0.0")]          // requires knowledge in attributes/reflections
+
+//----4 Assembly IDENTIFYER---
+// 1- Name
+// 2- Version
+// 3- Public Key
+// 4- Culture
+
+// csc /out:Farm.dll    -- basic name
+
+public class Cow
+{
+    public static void Moo()
+    {
+        Console.WriteLine("Moo Version 1");
+    }
+}
